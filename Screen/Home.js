b646686232eba0d8240f319d641f3d9d0dd16145
Your Name <quytvgcd17298@fpt.edu.vn>
2021-10-11 22:14:46 +0700

    Fetch API from server

--- Screen/Home.js
+++ Screen/Home.js
@@ -1,52 +1,50 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Listing from '../components/Listing';
 const Home = () => {
-    const [data, setData] = useState([
-        {
-            id: 1,
-            title: "Canalside house",
-            thumbnail: "https://picsum.photos/id/164/400/400",
-            price: "$350,000",
-          },
-          {
-            id: 2,
-            title: "Old center townhouse",
-            thumbnail: "https://picsum.photos/id/405/400/400",
-            price: "$800,000",
-          },
-          {
-            id: 3,
-            title: "Centrally located appartment",
-            thumbnail: "https://picsum.photos/id/1065/400/400",
-            price: "$280,000",
-          },
-          {
-            id: 4,
-            title: "Downtown apartment",
-            thumbnail: "https://picsum.photos/id/1076/400/400",
-            price: "$1,200,000",
-          },
-          {
-            id: 5,
-            title: "Fairytale castle",
-            thumbnail: "https://picsum.photos/id/1040/400/400",
-            price: "$5,000,000",
-          },
-      ]);
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        return () => {
+          FetchAPI();
+        }
+      }, []);
+    const FetchAPI = async() => {
+      setLoading(true);
+      const data = await fetch(
+      "https://my-json-server.typicode.com/PacktPublishing/React-Projects/listings");
+      const dataJSON = await data.json();
+      setData(dataJSON);
+      setLoading(false);
+    };
+
     return (
         <View
         style ={styles.style}>
-            <Text style ={{fontSize: 30}}>House List</Text>
+          {loading? (
+            <Text
+            style = {{fontSize: 40, color: "blue"}}>Loading..</Text>
+          ) : ( 
+            <View
+            style = {{
+              justifyContent:"center",
+              alignItems:"center"
+              }}>
+            <Text
+            style = {{fontSize:30 , color:"blue"}}
+            >House List</Text>
             <FlatList
              keyExtractor={(item) => String(item.id)}
              data={data}
              renderItem={({item}) => <Listing item={item} />}
             ></FlatList>
+            </View>
+          )
+          }
         </View>
-    )
-}
+    );
+};
 export default Home
 
 const styles = StyleSheet.create({
